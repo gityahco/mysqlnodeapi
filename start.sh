76b6d8f1ff7d62906f6dcf3ebc85e8a1f49ff893
Yahya Mardini <120016906+gityahco@users.noreply.github.com>
2024-03-01 21:54:09 +0300

    editing start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 # Start mysql docker compose
-cd mysql/
-pwd
+cd mysql
 docker compose up -d
-cd ..
-pwd
 
 # wait until mysql docker compose is fully initialized
 while true; do
     if docker compose logs | grep -q "ready for connections"; then
         echo "Initialization done"
         sleep 5
+        cd ..
         # Change directory to 'api' and start its Docker compose
         cd api
-        pwd
         docker compose up
         break
     else
